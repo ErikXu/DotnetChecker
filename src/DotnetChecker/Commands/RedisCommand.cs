bff6a843f8f54f403e4beb282522b163298cc689
xuzhe <xuzhe@q1.com>
2020-06-29 19:29:06 +0800

    Add redis stack exchange check.

---
+++ src/DotnetChecker/Commands/RedisCommand.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using McMaster.Extensions.CommandLineUtils;
+using StackExchange.Redis;
+
+namespace DotnetChecker.Commands
+{
+    [Command("redis", Description = "Check redis")]
+    public class RedisCommand
+    {
+        private const string StackExchange = "StackExchange";
+        private const string CsRedis = "CsRedis";
+
+        [Option("-d|--Driver", "Redis driver, " + StackExchange + " or " + StackExchange + ", default " + StackExchange, CommandOptionType.SingleOrNoValue)]
+        public string Type { get; set; }
+
+        [Option("-c|--Connection", "Redis connection string, example: localhost:6379", CommandOptionType.SingleValue)]
+        public string Connection { get; set; }
+
+        private readonly IConsole _console;
+
+        public RedisCommand(IConsole console)
+        {
+            _console = console;
+        }
+
+        private void OnExecute()
+        {
+            if (string.IsNullOrWhiteSpace(Connection))
+            {
+                _console.WriteLine("The connection can not be null.");
+                return;
+            }
+
+            switch (Type)
+            {
+                case CsRedis:
+                    break;
+                default:
+                    try
+                    {
+                        CheckStackExchange(Connection);
+                    }
+                    catch (Exception ex)
+                    {
+                        _console.WriteLine("Error occurs.");
+                        _console.WriteLine(ex);
+                    }
+                   
+                    break;
+            }
+        }
+
+        private void CheckStackExchange(string connection)
+        {
+            var conn = ConnectionMultiplexer.Connect(connection);
+
+            var db = conn.GetDatabase();
+
+            var key = $"dotnet-checker-{Guid.NewGuid()}";
+            db.StringSet(key, "OK");
+            _console.WriteLine("StringSet is normal.");
+
+            var value = db.StringGet(key);
+            _console.WriteLine($"StringGet is normal, value is {value}.");
+
+            var isOk = db.KeyDelete(key);
+            if (isOk)
+            {
+                _console.WriteLine("KeyDelete is normal.");
+            }
+        }
+    }
+}
---
+++ src/DotnetChecker/Program.cs
@@ -0,0 +1,45 @@
+﻿using DotnetChecker.Commands;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotnetChecker
+{
+    [HelpOption(Inherited = true)]
+    [Command(Description = "A tool to check .net core capable."),
+     Subcommand(typeof(RedisCommand))]
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddSingleton(PhysicalConsole.Singleton);
+
+            var services = serviceCollection.BuildServiceProvider();
+
+            var app = new CommandLineApplication<Program>();
+            app.Conventions
+                .UseDefaultConventions()
+                .UseConstructorInjection(services);
+
+            var console = (IConsole)services.GetService(typeof(IConsole));
+
+            try
+            {
+                return app.Execute(args);
+            }
+            catch (UnrecognizedCommandParsingException ex)
+            {
+                console.WriteLine(ex.Message);
+                return -1;
+            }
+        }
+
+        private int OnExecute(CommandLineApplication app, IConsole console)
+        {
+            console.WriteLine("Please specify a command.");
+            app.ShowHelp();
+            return 1;
+        }
+    }
+}
